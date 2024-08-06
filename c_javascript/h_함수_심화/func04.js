f69fd9cd67538e97d27eb0c72aab80aa5a6df26d
sueun0812 <plok9408@naver.com>
2024-08-06 12:49:46 +0900

    20240806

--- c_javascript/h_함수_심화/func04.js
+++ c_javascript/h_함수_심화/func04.js
@@ -1,1 +1,59 @@
 // # func04.js
+
+// 자바스크립트의 내장 함수 - 콜백 함수 활용 {고차 함수}
+
+// ! 타이머 함수
+// : 특정한 시간이 지난 후 함수를 실행
+//   or 일정 간격으로 함수를 반복 실행하는 기능
+
+// & 1. setTimeout(콜백함수, 시간);
+// 지정된 시간이 지난 후, 주어진 콜백 함수를 단 한 번 실행
+// - (지연) 시간 : 밀리초(ms, 1000밀리초 === 1초)
+
+// * 기본 사용법
+function greet(name='정수은') {
+  console.log(`hello, ${name}`);
+}
+
+greet();
+
+// 2초 뒤에 실행
+// >> 콜백함수로 함수를 전달
+// >> 콜백함수의 인자 전달은 세 번째 인자부터 나열되는 값이 함수의 매개변수로 전달
+setTimeout(greet, 2000);    // 2초 뒤 실행
+setTimeout(greet, 3000, '쪼꼬');    // 3초 뒤 실행
+
+// * 익명함수 사용
+setTimeout(function() {
+  console.log('4초 뒤에 실행됩니다.');
+}, 4000);
+
+setTimeout(function() {
+  console.log('5초 뒤에 실행됩니다.');
+}, 5000);
+
+// cf) 타이머 취소
+// : clearTimeout(타이머_ID) 
+// : setTimeout으로 설정한 타이머 제거
+
+// >> setTimeout()은 타이머 ID를 자동 반환
+
+const timeID = setTimeout(greet, 2000, '수은정');
+
+clearTimeout(timeID);
+
+// & 2. setInterval(콜백함수, 시간);
+// '지정된 시간 간격마다' 함수를 반복해서 실행
+// : 사용법 setTimeout()과 유사
+
+let count = 1;
+
+let id = setInterval(() => {
+  console.log(`3초 마다 실행됩니다. ${count}번째`);
+  count++;
+}, 3000); // 3000밀리초 (3초)
+
+setTimeout(() => {
+  console.log(`타이머를 종료합니다.`);
+  clearInterval(id);
+}, 15000);  // 15000밀리초(15초)
---
+++ c_javascript/h_함수_심화/practice01.js
@@ -0,0 +1,59 @@
+// # practice01.js
+
+// ! 배열 메서드 + 콜백 함수
+
+// # 1번 문제
+// 1. scores 배열을 생성
+// 2. 무작위의 2자릿수 데이터를 8개 이상 채우기
+const scores = [44, 55, 66, 77, 88];
+
+// 3. passingScores 함수
+// - 60점 이상인 점수의 개수를 passingCount 변수에 저장
+// - 60점 이상인 점수의 평균을 passingAverage 변수에 저장
+// >> 두개의 데이터를 반환 (return a, b)
+function passingScores(socre) {
+  // 60점 이상인 점수를 필터링
+  const passing = scores.filter(score => score >= 60);
+  
+  // 60점 이상인 점수의 개수
+  const passingCount = passing.length;
+
+  // 60점 이상인 점수들의 평균
+  const passingAverage = passing.reduce((acc, score) => acc + score, 0) / passingCount;
+
+  // 각각의 변수값을 하나의 배열로 변환
+  return [passingCount, passingAverage];
+}
+
+// 4. 함수 호출 (콘솔에 출력)
+
+// ? cf) 구조분해할당
+// : 배열이나 객체의 데이터를 각각의 변수에 맞춰 할당
+const [passingCount, passingAverage] = passingScores(scores);
+// 구조분해할당된 배열 안의 각 요소는 개별적인 변수와 동일
+
+console.log(`60점 이상 점수의 개수 : ${passingCount}`); // 3
+console.log(`60점 이상 점수의 평균 : ${passingAverage}`); // 77
+
+
+// # 2번 문제
+const numbers = [1, 2, 3, 4, 5];
+// map 사용
+// : 주어진 숫자 배열의 각 요소에 10을 더한 새로운 배열을 생성
+const addedTen = numbers.map(num => num + 10);
+console.log(addedTen);  // [11, 12, 13, 14, 15]
+
+// filter 사용
+// : 주엉진 ㄴ숫자 배열에서 홀수만 추출하여 새로운 배열을 생성
+const odds = addedTen.filter(num => num % 2 !== 0);
+console.log(odds);  // [ 11, 13, 15 ]
+
+// 3) map과 filter 함께 사용
+// : 주어진 숫자 배열에서 짝수만 찾아 각 숫자에 5를 곱한 새로운 배열 생성
+const numbers2 = [1, 2, 3, 4, 5, 6];
+
+const evenTimesFive = numbers2
+  .filter(number => number % 2 === 0) // 2, 4, 6
+  .map(number => number * 5); // 10, 20, 30
+
+  console.log(evenTimesFive); // [ 10, 20, 30 ]
